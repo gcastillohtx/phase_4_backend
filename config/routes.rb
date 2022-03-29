63090809b07c2b24dd85b16013c0a0d4c2923d4a
gianv322 <90941304+gianv322@users.noreply.github.com>
2022-03-29 17:20:15 -0400

    Finished Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :liked_events
-  resources :users
-  resources :events
+  resources :liked_events, only: [:index, :destroy]
+  resources :users, only: [:show, :destroy, :create]
+  resources :events, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-event1 = Event.create(name: "New York Knicks at the Brooklyn Nets", time: "8:30 pm EST", date: "03/28/22", location: "Barclays Center in Brooklyn, NY", sport: "NBA", where_to_watch: "YES Network" )
+event1 = Event.create(name: "New York Knicks at the Brooklyn Nets", time: "8:30 pm EST", date: "04/28/22", location: "Barclays Center in Brooklyn, NY", sport: "NBA", where_to_watch: "YES Network" )
 event2 = Event.create(name: "Philadelphia Eagles at the New York Giants", time: "1:00 pm EST", date: "10/14/22", location: "MetLife Stadium in East Rutherford, NJ", sport: "NFL", where_to_watch: "FOX" )
 event3 = Event.create(name: "Las Vegas Aces at the New York Liberty", time: "9:00 pm EST", date: "12/03/22", location: "Barclays Center in Brooklyn, NY", sport: "WNBA", where_to_watch: "YES Network" )
+event4 = Event.create(name: "Boston Red Sox at the New York Yankees", time: "4:00 pm EST", date: "05/12/22", location: "Yankee Stadium in The Bronx, NY", sport: "MLB", where_to_watch: "YES Network" )
+event5 = Event.create(name: "Toronto Mapleleaf at the Boston Bruins", time: "10:30 pm EST", date: "09/18/22", location: "TD Garden in Boston, MA", sport: "NHL", where_to_watch: "NHL Network" )
+event6 = Event.create(name: "Los Angeles Rams at the Arizona Cardinals", time: "1:00 pm EST", date: "10/31/22", location: "State Farm Stadium in Glendale, AZ", sport: "NFL", where_to_watch: "CBS" )
+event7 = Event.create(name: "Milwakee Bucks at the Philadelphia 76ers", time: "7:00 pm EST", date: "04/28/22", location: "Wells Fargo Center in Philadelphia, PA", sport: "NBA", where_to_watch: "NBC" )
+event8 = Event.create(name: "Cleveland Browns at the Pittsburgh Steelers", time: "8:15 pm EST", date: "01/03/23", location: "Heinz Field in Pittsburgh, PA", sport: "NFL", where_to_watch: "FOX" )
+event9 = Event.create(name: "Chicago Sky at the Phoenix Mercury", time: "7:30 pm EST", date: "10/10/22", location: "Footprint Center in Phoenix, AZ", sport: "WNBA", where_to_watch: "TNT" )
+event10 = Event.create(name: "Los Angeles Lakers at the Dallas Mavericks", time: "7:30 pm EST", date: "04/02/22", location: "American Airlines Arena in Dallas, TX", sport: "NBA", where_to_watch: "TNT" )
+event11 = Event.create(name: "Carolina Hurricanes at the Tampa Bay Lightning", time: "7:00 pm EST", date: "04/11/22", location: "Amalie Arena in Tampa Bay, FL", sport: "NHL", where_to_watch: "CBS" )
+event12 = Event.create(name: "Las Vegas Aces at the New York Liberty", time: "9:00 pm EST", date: "12/03/22", location: "Barclays Center in Brooklyn, NY", sport: "WNBA", where_to_watch: "YES Network" )
+event13 = Event.create(name: "New York Knicks at the Brooklyn Nets", time: "8:30 pm EST", date: "03/28/22", location: "Barclays Center in Brooklyn, NY", sport: "NBA", where_to_watch: "YES Network" )
+event14 = Event.create(name: "Philadelphia Eagles at the New York Giants", time: "1:00 pm EST", date: "10/14/22", location: "MetLife Stadium in East Rutherford, NJ", sport: "NFL", where_to_watch: "FOX" )
+event15 = Event.create(name: "Las Vegas Aces at the New York Liberty", time: "9:00 pm EST", date: "12/03/22", location: "Barclays Center in Brooklyn, NY", sport: "WNBA", where_to_watch: "YES Network" )
 
 user1 = User.create(name: "Kevin Durant", location: "Brooklyn, NY")
 user2 = User.create(name: "Lebron James", location: "Los Angeles, CA")
