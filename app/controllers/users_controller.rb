c1e4dc5c9165aa0be4f40ce552ded87c95ddda11
gianv322 <90941304+gianv322@users.noreply.github.com>
2022-03-30 14:48:35 -0400

    Require Password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.permit(:name, :location)
+        params.permit(:name, :location, :password)
     end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+    
     has_many :liked_events
     has_many :events, through: :liked_events
 
     validates :name, presence: true
     validates :location, presence: true
+    validates :password, presence: true
 end
